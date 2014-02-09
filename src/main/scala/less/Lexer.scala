22c8b47b83edfd85a66080368cea2b73fa735c5c
Kevin Schuetz <schuetzk@gmail.com>
2014-02-09 13:59:14 -0600

    comments, numbers, variables

--- src/main/scala/less/Lexer.scala
+++ src/main/scala/less/Lexer.scala
@@ -2,6 +2,7 @@ package com.kschuetz.less
 
 import source._
 import java.io.Reader
+import scala.util.Try
 
 
 object LessLexerState {
@@ -10,14 +11,13 @@ object LessLexerState {
     type S_ = Value
 
     val START, NUMBER, NUMDOT, FLOAT, AT, AT_IDENT, SLASH,
-        LINECOMMENT, BLOCKCOMMENT, BLOCKCOMMENTSTAR,
-        STRING_LIT, LIT_ESC, LIT_AT, LIT_AT_BRACE, LIT_INTERP_IDENT, LIT_INTERP_IDENT_END  = Value
+    LINECOMMENT, BLOCKCOMMENT, BLOCKCOMMENTSTAR,
+    STRING_LIT, LIT_ESC, LIT_AT, LIT_AT_BRACE, LIT_INTERP_IDENT, LIT_INTERP_IDENT_END  = Value
   }
 
 }
 
-class LessLexerState(reader: CharReader, makeSourcePos: (Int, Int) => Position) {
-
+private class LessLexerState(reader: CharReader, makeSourcePos: (Int, Int) => Position) {
 
   sealed abstract trait TokenResult
   case class Success(token: LexerToken) extends TokenResult
@@ -42,50 +42,53 @@ class LessLexerState(reader: CharReader, makeSourcePos: (Int, Int) => Position) 
   def lexerToken(token: Token, line: Int, col: Int): LexerToken =
     LexerToken(token, makeSourcePos(line, col))
 
-  private def scan: TokenResult = {
-    import LessLexerState.S_._
 
-    type StateType = LessLexerState.S_.Value
+  object State {
+    import tokens._
 
-    var state = START
-    var found = false
+    var done = false
     var result: TokenResult = NothingLeft
     var startLine: Int = 0
     var startCol: Int = 0
     var itemCount: Int = 0
     var subItemOffset: Int = 0;
-    var qChar: Char = '\0';
+    var qChar: Char = '\0'
+    var handler: (Option[SourceChar] => Unit) = top
 
-
-    val capture = new StringBuilder
+    var capture: StringBuilder = null
 
     val tokenBuffer = new collection.mutable.ArrayBuffer[LexerToken]
 
-
     def accept(token: Token, line: Int, col: Int): Unit = {
       result = Success(LexerToken(token, makeSourcePos(line, col)))
-      found = true
+      done = true
     }
 
     def acceptMany(tokens: Vector[LexerToken]): Unit = {
       result = SuccessMany(tokens)
-      found = true
+      done = true
     }
 
     def error(msg: String, line: Int, col: Int): Unit = {
       val e = ParseError(msg, makeSourcePos(line, col))
       result = Failure(e)
-      found = true
+      done = true
     }
 
-    def markBegin(line: Int, col: Int) = {
-      startLine = line
-      startCol = col
+    def error(msg: String, sourceChar: Option[SourceChar]): Unit = {
+      val (line, col) = toLineCol(sourceChar)
+      val e = ParseError(msg, makeSourcePos(line, col))
+      result = Failure(e)
+      done = true
     }
 
-    def startCapture(c: Char) = {
-      capture.clear()
-      capture.append(c)
+    /**
+     * Extracts line and col from a Some(SourceChar), or
+     * from the reader position if None (which will point to the
+     * position at the end of input)
+     */
+    def toLineCol(sc: Option[SourceChar]): (Int, Int) = {
+      sc.map { x => (x.line, x.col) } getOrElse { (reader.line, reader.col) }
     }
 
     def unterminatedBlockCommentError(line: Int, col: Int): Unit =
@@ -95,274 +98,174 @@ class LessLexerState(reader: CharReader, makeSourcePos: (Int, Int) => Position) 
     def unterminatedStringLiteralError(line: Int, col: Int): Unit =
       error("Unterminated string literal", line, col)
 
-    def addStringLiteralChunks(): Unit = {
-      import tokens._
-
-      val s = capture.result()
-      if(subItemOffset > 0) {
-        tokenBuffer += lexerToken(StringLiteralChunk(s.take(subItemOffset)), startLine, startCol)
-      }
-
-      val ident = s.substring(subItemOffset + 2, s.length - 1)
-      tokenBuffer += lexerToken(InterpolatedIdentifier(ident), startLine, startCol + subItemOffset)
-
+    def markBegin(line: Int, col: Int) = {
+      startLine = line
+      startCol = col
     }
 
-    def completeState(state: StateType, endOfInput: Boolean = false) {
-      import tokens._
+    def resetCapture(): Unit = {
+      if(capture==null) { capture = new StringBuilder }
+      else { capture.clear() }
+    }
 
-      state match {
-        case SLASH => accept(Slash, startLine, startCol)
 
-        case AT_IDENT => {
-          val ident = capture.result
+    def at1(input: Option[SourceChar]): Unit = {
+      var ok = input.isDefined
+      input.foreach { sc =>
+        if(sc.c == '@') { itemCount += 1 }
+        else if (isValidIdentChar(sc.c)) {
+          resetCapture()
+          capture.append(sc.c)
+          handler = atIdent
+        } else ok = false
+      }
+      if(!ok) error("@ must be followed by a variable name or directive", startLine, startCol)
+    }
 
-          val ts = (0 until itemCount).map { n =>
-            lexerToken(At, startLine, startCol + n)
-          }.toVector :+ lexerToken(Identifier(ident), startLine, startCol + itemCount)
-
-          acceptMany(ts)
-        }
-
-        case BLOCKCOMMENT => {
-          if(!endOfInput) {
-            val s = capture.result
-            accept(BlockComment(s), startLine, startCol)
-          }
-          else unterminatedBlockCommentError(reader.line, reader. col)
-        }
-
-        case BLOCKCOMMENTSTAR => unterminatedBlockCommentError(reader.line, reader. col)
-
-        case LINECOMMENT => {
-          val s = capture.result
-          accept(InlineComment(s), startLine, startCol)
-        }
-
-        case STRING_LIT => {
-          val s = capture.result
-          val t = StringLiteralChunk(s)
-          if(tokenBuffer.isEmpty) {
-            accept(t, startLine, startCol)
-          } else {
-            tokenBuffer += lexerToken(t, startLine, startCol)
-            acceptMany(tokenBuffer.toVector)
-          }
-        }
-        case LIT_ESC => unterminatedStringLiteralError(reader.line, reader. col)
-        case LIT_AT => unterminatedStringLiteralError(reader.line, reader. col)
-        case LIT_AT_BRACE => unterminatedStringLiteralError(reader.line, reader. col)
-        case LIT_INTERP_IDENT if endOfInput => unterminatedStringLiteralError(reader.line, reader. col)
-        case LIT_INTERP_IDENT => {
-          //val s = capture.result
-          //if
-
-        }
+    def atIdent(input: Option[SourceChar]): Unit = {
+      var more = false
+      input.foreach { sc =>
+        if(isValidIdentChar(sc.c)) { more = true; capture.append(sc.c)}
+        else { reader.unget(sc) }
+      }
+      if(!more) {
+        val ident = capture.result
+        val ts = (0 until itemCount).map { n =>
+          lexerToken(At, startLine, startCol + n)
+        }.toVector :+ lexerToken(Identifier(ident), startLine, startCol + itemCount)
+        acceptMany(ts)
       }
     }
 
-    while(!found) {
-      reader.get.map { case pc @ SourceChar(c, line, col) =>
-        import tokens._
+    def lineComment(input: Option[SourceChar]): Unit = {
+      var more = false
+      input.foreach { sc =>
+        if(!isLineBreak(sc.c)) { more = true; capture.append(sc.c) }
+      }
+      if(!more) {
+        val s = capture.result
+        accept(InlineComment(s), startLine, startCol)
+      }
+    }
 
-        if(state == START) {
-          c match {
-            case '>' =>   accept(Gt, line, col)
-            case '{' =>   accept(LBrace, line, col)
-            case '}' =>   accept(RBrace, line, col)
-            case '[' =>   accept(LBracket, line, col)
-            case ']' =>   accept(RBracket, line, col)
-            case '=' =>   accept(Eq, line, col)
-            case ';' =>   accept(Semicolon, line, col)
-            case ':' =>   accept(Colon, line, col)
-            case '-' =>   accept(Minus, line, col)
-            case '+' =>   accept(Plus, line, col)
-            case '*' =>   accept(Star, line, col)
-            case '(' =>   accept(LParen, line, col)
-            case ')' =>   accept(RParen, line, col)
-            case ',' =>   accept(Comma, line, col)
-            case '.' =>   accept(Dot, line, col)
-            case '#' =>   accept(Hash, line, col)
-            case ch if isStringLiteralDelimiter(ch) => {
-              qChar = ch
-              markBegin(line, col)
-              itemCount = 0
-              capture.clear()
-              tokenBuffer.clear()
-              state = STRING_LIT
-            }
-            case n if n.isDigit => {
-              state = NUMBER
-              markBegin(line, col)
-              startCapture(c)
-            }
-            case '@' => {
-              state = AT
-              itemCount = 1
-              markBegin(line, col)
-            }
-            case '/' => {
-              state = SLASH
-              markBegin(line, col)
-            }
-          }
-
-        } else if (state == AT) {
-          c match {
-            case '@' =>   itemCount += 1
-            case ch if isValidIdentChar(ch) => {
-              startCapture(ch)
-              state = AT_IDENT
-            }
-            case _ => error("@ must be followed by a variable name or directive", startLine, startCol)
-          }
-
-        } else if (state == AT_IDENT) {
-          c match {
-            case ch if isValidIdentChar(ch) => capture.append(ch)
-            case _ => {
-              reader.unget(pc)
-              completeState(state)
-            }
-          }
-
-        } else if (state == SLASH) {
-          // slash
-          c match {
-            case '/' => {
-              state = LINECOMMENT
-              capture.clear()
-            }
-            case '*' => {
-              state = BLOCKCOMMENT
-              capture.clear()
-            }
-            case _ => {
-              reader.unget(pc)
-              completeState(state)
-            }
-          }
-
-        } else if (state == LINECOMMENT) {
-          if(isLineBreak(c)) {
-            completeState(state)
-          } else {
-            capture.append(c)
-          }
-
-        } else if (state == BLOCKCOMMENT) {
-          if(c == '*') state = BLOCKCOMMENTSTAR
-          else capture.append(c)
-
-        } else if (state == BLOCKCOMMENTSTAR) {
-          if(c == '/') {
-            completeState(BLOCKCOMMENT)
-          } else {
-            capture.append('*')
-            capture.append(c)
-            state = BLOCKCOMMENT
-          }
-
-        } else if (state == NUMBER) {
-          c match {
-            case '.' => {
-              capture.append('.')
-              state = NUMDOT
-            }
-            case ch if ch.isDigit => capture.append(ch)
-            case _ => {
-              reader.unget(pc)
-              completeState(NUMBER)
-            }
-          }
-
-        } else if (state == NUMDOT) {
-          if(c.isDigit) {
-            capture.append(c)
-            state = FLOAT
-          } else {
-            error("Expected digit", line, col)
-          }
-
-        } else if (state == FLOAT) {
-          if (c.isDigit) {
-            capture.append(c)
-          } else {
-            reader.unget(pc)
-            completeState(FLOAT)
-          }
-
-        } else if (state == STRING_LIT) {
-          if (c == qChar) {
-            completeState(state)
-          } else if (isLineBreak(c)) {
-            unterminatedStringLiteralError(line, col)
-          } else {
-            capture.append(c)
-            c match {
-              case '@' => {
-                state = LIT_AT
-                subItemOffset = itemCount
-              }
-              case '\\' => state = LIT_ESC
-            }
-            itemCount += 1
-          }
-        } else if (state == LIT_ESC) {
-
-          if(isLineBreak(c)) unterminatedStringLiteralError(line, col)
-          else {
-            capture.append(c)
-            if(c == '@') {
-              state = LIT_AT
-              subItemOffset = itemCount
-            }
-            itemCount += 1
-          }
-
-        } else if ((state == LIT_AT) || (state == LIT_AT_BRACE) || (state == LIT_INTERP_IDENT)) {
-
-          if(c == qChar) {
-            completeState(STRING_LIT)
-          } else if (isLineBreak(c)) {
-            unterminatedStringLiteralError(line, col)
-          } else if (c == '\\') {
-            capture.append(c)
-            itemCount += 1
-            state = LIT_ESC
-          } else {
-            capture.append(c)
-            itemCount += 1
-            state match {
-              case LIT_AT => if(c == '{') { state = LIT_AT_BRACE } else { state = STRING_LIT }
-              case LIT_AT_BRACE => if(isValidIdentStartChar(c)) { state = LIT_INTERP_IDENT } else { state = STRING_LIT }
-              case LIT_INTERP_IDENT => {
-                if(c == '}') { state = LIT_INTERP_IDENT_END }
-                else if (!isValidIdentChar(c)) { state = STRING_LIT }
-              }
-            }
-          }
-
-        } else if (state == LIT_INTERP_IDENT_END) {
-          if(c == qChar) {
-            completeState(state)
-          } else {
-            addStringLiteralChunks()
-            reader.unget(pc)
-            markBegin(line, col)
-            itemCount = 0
-            capture.clear()
-            state = STRING_LIT
-          }
-
-        }
-
+    def blockComment(input: Option[SourceChar]): Unit = {
+      input.map { sc =>
+        if(sc.c == '*') handler = blockCommentStar
+        else capture.append(sc.c)
       } getOrElse {
-        completeState(state, true)
+        unterminatedBlockCommentError(reader.line, reader.col)
       }
     }
-    result
+
+    def blockCommentStar(input: Option[SourceChar]): Unit = {
+      input.map { sc =>
+        if(sc.c == '/') {
+          val s = capture.result
+          accept(BlockComment(s), startLine, startCol)
+        } else {
+          capture.append('*')
+          capture.append(sc.c)
+          handler = blockComment
+        }
+      } getOrElse {
+        unterminatedBlockCommentError(reader.line, reader.col)
+      }
+    }
+
+    def slash(input: Option[SourceChar]): Unit = {
+      var more = false
+      input.foreach { sc =>
+        sc.c match {
+          case '/' => { resetCapture(); more = true; handler = lineComment }
+          case '*' => { resetCapture(); more = true; handler = blockComment }
+          case _ => { reader.unget(sc) }
+        }
+      }
+      if(!more) accept(Slash, startLine, startCol)
+    }
+
+
+    def number(input: Option[SourceChar]): Unit = {
+      var more = false
+      input.foreach { sc =>
+        if(sc.c.isDigit) { more = true; capture.append(sc.c) }
+        else if(sc.c == '.') { more = true; capture.append('.'); handler = numberDot }
+        else reader.unget(sc)
+      }
+      if(!more) {
+        val s = capture.result
+        accept(WholeNumber(BigInt(s)), startLine, startCol)
+      }
+    }
+
+    def numberDot(input: Option[SourceChar]): Unit = {
+      var ok = false
+      input.foreach { sc =>
+        if(sc.c.isDigit) { ok = true; capture.append(sc.c); handler = numberFloat }
+      }
+      if(!ok) error("Expected digit", input)
+    }
+
+    def numberFloat(input: Option[SourceChar]): Unit = {
+      var more = false
+      input.foreach { sc =>
+        if(sc.c.isDigit) { more = true; capture.append(sc.c) }
+        else reader.unget(sc)
+      }
+      if(!more) {
+        val s = capture.result()
+        Try(s.toDouble).toOption match {
+          case Some(d) => accept(FloatNumber(d), startLine, startCol)
+          case _ => error("Could not parse floating point number", startLine, startCol)
+        }
+      }
+    }
+
+    def stringLiteral(input: Option[SourceChar]): Unit = {
+
+    }
+
+    def stringLiteralEsc(input: Option[SourceChar]): Unit = {
+
+    }
+
+    def stringLiteralAt(input: Option[SourceChar]): Unit = {
+
+    }
+
+    def stringLiteralAtBrace(input: Option[SourceChar]): Unit = {
+
+    }
+
+    def stringLiteralInterpolate(input: Option[SourceChar]): Unit = {
+
+    }
+
+
+    def top(input: Option[SourceChar]): Unit = {
+      input.foreach { case sc @ SourceChar(c, line, col) =>
+        c match {
+          case '@' => { itemCount = 1; markBegin(line, col); handler = at1 }
+          case '/' => { markBegin(line, col); handler = slash }
+          case n if n.isDigit => { markBegin(line, col); resetCapture(); capture.append(n); handler = number }
+        }
+      }
+    }
+
   }
+
+
+
+
+  def scan: TokenResult = {
+    import State._
+    while(!done) {
+      val sc = reader.get
+      handler(sc)
+    }
+    State.result
+  }
+
 
   def next: Stream[Either[LexerError, LexerToken]] = {
     scan match {
@@ -370,7 +273,7 @@ class LessLexerState(reader: CharReader, makeSourcePos: (Int, Int) => Position) 
         Right(t) #:: (new LessLexerState(reader, makeSourcePos)).next
       }
       case SuccessMany(ts) => {
-        ts.foldRight((new LessLexerState(reader, makeSourcePos)).next){ Right(_) #:: _ }
+         ts.foldRight((new LessLexerState(reader, makeSourcePos)).next){ Right(_) #:: _ }
       }
       case Failure(e) => {
         reader.close()
@@ -382,7 +285,6 @@ class LessLexerState(reader: CharReader, makeSourcePos: (Int, Int) => Position) 
       }
     }
   }
-
 }
 
 
