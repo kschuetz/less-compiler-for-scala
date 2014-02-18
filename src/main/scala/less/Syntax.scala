0758cfc68107f796082423b14767e6dbc9d6c299
Kevin Schuetz <schuetzk@gmail.com>
2014-02-17 23:06:49 -0600

    defined data types for dimension units

--- src/main/scala/less/Syntax.scala
+++ src/main/scala/less/Syntax.scala
@@ -31,5 +31,59 @@ object syntax {
   case class UrlQuoted(value: StringLiteral) extends UrlExpression
   case class UrlUnquoted(value: String) extends UrlExpression
 
+  sealed abstract trait DimensionUnit {
+    def name: String
+  }
+
+  sealed abstract trait LengthUnit extends DimensionUnit
+
+  sealed abstract trait RelativeLengthUnit extends LengthUnit
+
+  case object EmUnit extends RelativeLengthUnit { val name = "em" }
+  case object RemUnit extends RelativeLengthUnit { val name = "rem" }
+  case object ExUnit extends RelativeLengthUnit { val name = "ex" }
+  case object ChUnit extends RelativeLengthUnit { val name = "ch" }
+  case object VwUnit extends RelativeLengthUnit { val name = "vw" }
+  case object VhUnit extends RelativeLengthUnit { val name = "vh" }
+  case object VminUnit extends RelativeLengthUnit { val name = "vmin" }
+  case object VmaxUnit extends RelativeLengthUnit { val name = "vmax" }
+
+  sealed abstract trait AbsoluteLengthUnit extends LengthUnit
+
+  case object PxUnit extends AbsoluteLengthUnit { val name = "px" }
+  case object CmUnit extends AbsoluteLengthUnit { val name = "cm" }
+  case object MmUnit extends AbsoluteLengthUnit { val name = "mm" }
+  case object InUnit extends AbsoluteLengthUnit { val name = "in" }
+  case object PtUnit extends AbsoluteLengthUnit { val name = "pt" }
+  case object PcUnit extends AbsoluteLengthUnit { val name = "pc" }
+
+  sealed abstract trait AngleUnit extends DimensionUnit
+  case object DegUnit extends AngleUnit { val name = "deg" }
+  case object GradUnit extends AngleUnit { val name = "grad" }
+  case object RadUnit extends AngleUnit { val name = "rad" }
+  case object TurnUnit extends AngleUnit { val name = "turn" }
+
+  sealed abstract trait TimeUnit extends DimensionUnit
+  case object SUnit extends TimeUnit { val name = "s" }
+  case object MsUnit extends TimeUnit { val name = "ms" }
+
+  sealed abstract trait FreqUnit extends DimensionUnit
+  case object HzUnit extends FreqUnit { val name = "Hz" }
+  case object KHzUnit extends FreqUnit { val name = "kHz" }
+
+  sealed abstract trait ResolutionUnit extends DimensionUnit
+  case object DpiUnit extends ResolutionUnit { val name = "dpi" }
+  case object DpcmUnit extends ResolutionUnit { val name = "dpcm" }
+  case object DppxUnit extends ResolutionUnit { val name = "dppx" }
+
+  case class UnknownUnit(name: String) extends DimensionUnit
+
+
+  sealed abstract trait NumberValue
+  case class WholeNumber(value: BigInt) extends NumberValue
+  case class FractionalNumber(value: Double, sourceRepr: String) extends NumberValue
+
+
+  case class DimensionValue(number: NumberValue, units: DimensionUnit)
 
 }
